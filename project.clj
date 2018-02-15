8b19851daea26cc51b3f071e365eac1bbe7b280a
Benjamin Esham <benjamin@esham.io>
2018-02-14 16:31:14 -0800

    Change description and reindent

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject hitting-set "1.0.0"
-            :description "Find minimal hitting sets"
-            :url "https://github.com/bdesham/hitting-set"
-            :license {:name "Eclipse Public License"
-                      :url "http://www.eclipse.org/legal/epl-v10.html"
-                      :distribution :repo
-                      :comments "Same as Clojure"}
-            :main hitting-set
-            :min-lein-version "2.0.0"
-            :dependencies [[org.clojure/clojure "1.4.0"]])
+  :description "Find hitting sets and set covers"
+  :url "https://github.com/bdesham/hitting-set"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "Same as Clojure"}
+  :main hitting-set
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
